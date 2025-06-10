35d3d5301f2f58fa1530a7df70afa4b1ad19ddf0
Prashmin Gurung <prashmingrg@gmail.com>
2025-06-11 07:53:04 +1000

    save work before Tailwind upgrade

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,46 +1,42 @@
+import Link from "next/link";
 import React from "react";
-import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuTrigger,
-  NavigationMenuContent,
-} from "@components/ui/navigation-menu";
+
+const navbarItems = [
+  {
+    href: "/",
+    label: "Home",
+  },
+
+  {
+    href: "/projects",
+    label: "Projects",
+  },
+  {
+    href: "/blog",
+    label: "Blog",
+  },
+  {
+    href: "/about",
+    label: "About",
+  },
+];
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex justify-center py-4 bg-background shadow">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuLink href="/">Home</NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="p-2">
-                <li>
-                  <NavigationMenuLink href="/projects/web">
-                    Web Projects
-                  </NavigationMenuLink>
-                </li>
-                <li>
-                  <NavigationMenuLink href="/projects/mobile">
-                    Mobile Projects
-                  </NavigationMenuLink>
-                </li>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink href="/about">About</NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink href="/contact">Contact</NavigationMenuLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </nav>
+    <>
+      <nav className="flex justify-center mx-auto max-w-7xl pt-4  bg-background">
+        <div className=" flex gap-x-8 ">
+          {navbarItems.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="text-gray-700 hover:text-blue-600 transition duration-200"
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </>
   );
 }
