c39806a041817a7b7c17c62ddc0665c64546186a
Prashmin Gurung <prashmingrg@gmail.com>
2025-06-26 13:54:03 +1000

    blog section is done, now fixing the error

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -11,7 +11,9 @@ export function getPostSlugs() {
 
 export function getPostBySlug(slug: string) {
   const realSlug = slug.replace(/\.md$/, "");
+
   const fullPath = join(postsDirectory, `${realSlug}.md`);
+
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
