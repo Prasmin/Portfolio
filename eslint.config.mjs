07cd26ee8397902109c0eb5da71e603c3e8ca0e4
Prashmin Gurung <prashmingrg@gmail.com>
2025-07-03 08:11:26 +1000

    ready for the production

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,21 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url); // Convert URL to filesystem path
+const __dirname = dirname(__filename); // Get directory name
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
